73549ce6ce0aa187a82ff0203b5fb3c8c5f3c037
Timofey <timma3a@gmail.com>
2020-01-08 20:49:53 +0500

    Version 1

--- project1.py
+++ project1.py
@@ -1,162 +1,100 @@
-import pygame
-import os
+import sqlite3
 import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLineEdit, QLCDNumber, QLabel, QCheckBox, QMainWindow, \
+    QPlainTextEdit, QFileDialog, QHBoxLayout, QVBoxLayout, QErrorMessage, QStackedLayout, QFrame
 
 
-def load_image(name, color_key=None):
-    fullname = os.path.join('data', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error as message:
-        print('Не удаётся загрузить:', name)
-        raise SystemExit(message)
-    image = image.convert_alpha()
-    if color_key is not None:
-        if color_key is -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    return image
-
-FPS = 50
-WIDTH = 400
-HEIGHT = 300
-STEP = 50
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
-
-
-class Camera:
-    # зададим начальный сдвиг камеры
+class Orders(QWidget):
     def __init__(self):
-        self.dx = 0
-        self.dy = 0
+        super().__init__()
+        self.initUI()
 
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
+    def initUI(self):
+        self.setGeometry(200, 150, 500, 500)
+        self.setWindowTitle('Заказ в Ресторане')
 
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+        self.DescriptionLabel = QPlainTextEdit('Программа оформления заказов в ресторане')
+        self.DescriptionLabel.setReadOnly(True)
+
+        self.Help1 = QLabel('Укажите путь до базы данных')
+
+        self.PathToDB = QLineEdit()
+
+        self.OpenDBFile = QPushButton('...')
+        self.OpenDBFile.clicked.connect(self.on_select_file)
+
+        self.StartBtn = QPushButton('Старт')
+        self.StartBtn.clicked.connect(self.on_start)
+
+        self.OpenFileLayout = QHBoxLayout()
+        self.OpenFileLayout.addWidget(self.PathToDB)
+        self.OpenFileLayout.addWidget(self.OpenDBFile)
+
+        self.FirstScreen = QVBoxLayout()
+        self.FirstScreen.addWidget(self.DescriptionLabel, alignment=Qt.AlignCenter)
+        self.FirstScreen.addWidget(self.Help1, alignment=Qt.AlignCenter)
+        self.FirstScreen.addLayout(self.OpenFileLayout)
+        self.FirstScreen.addWidget(self.StartBtn, alignment=Qt.AlignCenter)
+        self.FirstScreen.addStretch(1)
+
+        self.DateTime = QLineEdit()
+
+        self.ordernum = QLineEdit()
+
+        self.Header1 = QHBoxLayout()
+        self.Header1.addWidget(self.DateTime, alignment=Qt.AlignLeft | Qt.AlignTop)
+        self.Header1.addWidget(self.ordernum, alignment=Qt.AlignRight | Qt.AlignTop)
+
+        self.MainScreen = QVBoxLayout()
+        self.MainScreen.addLayout(self.Header1)
+
+        self.StLayout = QStackedLayout()
+
+        self.F1 = QFrame(self)
+        self.F1.setLayout(self.FirstScreen)
+
+        self.F2 = QFrame(self)
+        self.F2.setLayout(self.MainScreen)
+
+        self.StLayout.addWidget(self.F1)
+        self.StLayout.addWidget(self.F2)
+
+        self.setLayout(self.StLayout)
+
+    def on_select_file(self):
+        FileName, _ = QFileDialog.getOpenFileName(self, 'Открыть базу данных', None, '*.db')
+        if FileName:
+            self.PathToDB.setText(FileName)
+
+    def get_filename(self):
+        return self.PathToDB.text()
+
+    def on_start(self):
+        FileName = self.get_filename()
+        if FileName:
+            try:
+                con = sqlite3.connect(FileName)
+                cur = con.cursor()
+                result = cur.execute("""SELECT count(*) FROM meals""").fetchall()
+                if len(result) == 0:
+                    msg = QErrorMessage(self)
+                    msg.showMessage('В базе данных нет продуктов')
+                con.close()
+                self.start_main()
+            except Exception as e:
+                msg = QErrorMessage(self)
+                msg.showMessage('Неверный путь до базы данных. ' + str(e))
+        else:
+            msg = QErrorMessage(self)
+            msg.showMessage('Не указан путь до базы данных')
+
+    def start_main(self):
+        self.StLayout.setCurrentIndex(1)
 
 
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
-
-
-start_screen()
-tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png')}
-player_image = load_image('mario.png')
-
-tile_width = tile_height = 50
-# основной персонаж
-player = None
-
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-player, level_x, level_y = generate_level(load_level('map.txt'))
-camera = Camera()
-running = True
-
-lev = load_level('map.txt')
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and lev[(player.rect.y - 5) // 50][(player.rect.x - 15 - STEP) // 50] != '#':
-                player.rect.x -= STEP
-            if event.key == pygame.K_RIGHT and lev[(player.rect.y - 5) // 50][(player.rect.x + 15 + STEP) // 50] != '#':
-                player.rect.x += STEP
-            if event.key == pygame.K_UP and lev[(player.rect.y - STEP) // 50][(player.rect.x - 5) // 50] != '#':
-                player.rect.y -= STEP
-            if event.key == pygame.K_DOWN and lev[(player.rect.y + 15 + STEP) // 50][(player.rect.x - 5) // 50] != '#':
-                player.rect.y += STEP
-
-    screen.fill(pygame.Color(0, 0, 0))
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-
-    pygame.display.flip()
-
-    clock.tick(FPS)
-
-terminate()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Orders()
+    ex.show()
+    sys.exit(app.exec())
